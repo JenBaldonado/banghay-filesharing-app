620c3c4c0f04bc48c29a5c14739e5cc199f84c39
Jenza <spotty0326@gmail.com>
2022-12-08 23:01:15 +0800

    update

--- resources/views/layouts/reg.blade.php
+++ resources/views/layouts/reg.blade.php
@@ -82,7 +82,7 @@
             @yield('content')
         </main>
 
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 
     </html>
