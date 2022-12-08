034074a68f176e92d26c4db9c68bb7d50c9af594
Jenza <spotty0326@gmail.com>
2022-12-08 23:28:20 +0800

    testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -162,9 +162,9 @@
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-
-    @yield('script')
+<!--     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+ -->
+    
 
 
 </body>
